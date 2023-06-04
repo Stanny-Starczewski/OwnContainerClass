f99e6f23f7dda03185b58de7527d64354f19d402
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-06-04 03:09:40 -0400

    created a child controller and added it to the hierarchy

--- OwnContainerClass/ViewController.swift
+++ OwnContainerClass/ViewController.swift
@@ -17,6 +17,28 @@ class ViewController: UIViewController {
         bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         bottomView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
+        // Создадим экземпляр дочернего класса.
+        let childViewController = UIViewController()
+
+        // Добавляем дочерний контроллер в иерархию родителя.
+        addChild(childViewController)
+
+        // Добавляем `view` дочернего контроллера в иерархию родительской `view`.
+        childViewController.view.backgroundColor = .magenta
+        view.insertSubview(childViewController.view, belowSubview: bottomView)
+
+        // Добавим констрейнты.
+        if let childView = childViewController.view {
+            childView.translatesAutoresizingMaskIntoConstraints = false
+            childView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            childView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            childView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            childView.bottomAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
+        }
+
+        // Сообщаем системе, что процесс добавления дочернего контроллера завершён.
+        childViewController.didMove(toParent: self)
+        
     }
 
 
