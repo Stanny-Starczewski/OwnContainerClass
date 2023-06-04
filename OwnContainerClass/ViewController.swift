f65a09380e633bef4b689d12b8bac5de1b404204
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-06-04 02:53:03 -0400

    In the viewDidLoad method, added a yellow view with a height of 100 points below

--- OwnContainerClass/ViewController.swift
+++ OwnContainerClass/ViewController.swift
@@ -4,7 +4,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let bottomView = UIView()
+        bottomView.backgroundColor = .yellow
+        bottomView.layer.opacity = 1
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomView)
+        bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        bottomView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
     }
 
 
